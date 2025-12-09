387a0d17e34cf266f5108d0f464e9c5540ec2615
Jooydoooc <Jooydooc@gmail.com>
2025-12-10 00:13:49 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    document.getElementById('login-container').style.display = 'none';
+    document.getElementById('menu-container').style.display = 'block';
+}
+
+function openBook(book) {
+    alert('Opening ' + book);
+    // Here you would load or navigate to the book content
+}
